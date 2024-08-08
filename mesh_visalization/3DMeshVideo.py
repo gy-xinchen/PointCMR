a80c4c856c3593131c5f17c82c321fccab8e69fa
xinchen yuan <604658273@qq.com>
2024-08-08 15:55:42 +0800

    Update 3DMeshVideo.py

--- mesh_visalization/3DMeshVideo.py
+++ mesh_visalization/3DMeshVideo.py
@@ -4,25 +4,25 @@ import os
 from PIL import Image
 
 def rotate_pc_along_x_axis(pcd, angle_degrees):
-    """绕X轴旋转点云"""
+    """X axis rotate"""
     R = pcd.get_rotation_matrix_from_xyz((0, np.radians(angle_degrees), 0))
     pcd.rotate(R, center=(0, 0, 0))
     return pcd
 
 def rotate_pc_along_y_axis(pcd, angle_degrees):
-    """绕Y轴旋转点云"""
+    """Y axis rotate"""
     R = pcd.get_rotation_matrix_from_xyz((np.radians(angle_degrees), 0, 0))
     pcd.rotate(R, center=(0, 0, 0))
     return pcd
 
 def rotate_pc_along_z_axis(pcd, angle_degrees):
-    """绕Z轴旋转点云"""
+    """Z axis rotate"""
     R = pcd.get_rotation_matrix_from_xyz((0, 0, np.radians(angle_degrees)))
     pcd.rotate(R, center=(0, 0, 0))
     return pcd
 
 
-# 读取txt文件并生成点云
+# read point cloud from txt
 def load_point_cloud_from_txt(txt_path):
     pcd = np.genfromtxt(txt_path, delimiter=" ")
     pcd_vector = o3d.geometry.PointCloud()
@@ -30,14 +30,14 @@ def load_point_cloud_from_txt(txt_path):
     return pcd_vector
 
 
-# 获取文件夹中所有的txt文件
+# get txt from file
 def get_all_txt_files(folder_path):
     txt_files = [f for f in os.listdir(folder_path) if f.endswith('.txt')]
     txt_files.sort()  # 排序文件以确保顺序正确
     return txt_files
 
 
-# 生成并保存GIF
+# save GIF
 def generate_point_cloud_gif(folder_path, output_gif_path, fps=10):
     txt_files = get_all_txt_files(folder_path)
     vis = o3d.visualization.Visualizer()
@@ -57,7 +57,7 @@ def generate_point_cloud_gif(folder_path, output_gif_path, fps=10):
         vis.update_renderer()
         vis.capture_screen_image("temp.png")
 
-        # 打开并关闭图像文件，以便稍后删除
+        
         img = Image.open("temp.png")
         images.append(img.copy())
         img.close()
@@ -65,7 +65,7 @@ def generate_point_cloud_gif(folder_path, output_gif_path, fps=10):
     vis.destroy_window()
     os.remove("temp.png")
 
-    # 保存为GIF文件
+   
     images[0].save(output_gif_path, save_all=True, append_images=images[1:], duration=int(1000 / fps), loop=0)
 
 
