54ab440392683252b6f9f896b63dfde869381e6a
xinchen yuan <604658273@qq.com>
2024-08-08 15:51:45 +0800

    Update 3DMeshForSingleSlice.py

--- mesh_visalization/3DMeshForSingleSlice.py
+++ mesh_visalization/3DMeshForSingleSlice.py
@@ -1,13 +1,17 @@
 import open3d as o3d
 import numpy as np
 
-TxtPath = r"F:\袁心辰研究生资料\4DPH_anaylsis\4Dsegment-master\all_mPAP_4dnii_data0_60\Subject_0\motion\RV_fr00.txt"
+"""
+  to show point cloud [x,y,z]
+"""
 
-# 通过numpy读取txt点云
+TxtPath = r"...\Subject_0\motion\RV_fr00.txt"
+
+# read point cloud
 pcd = np.genfromtxt(TxtPath, delimiter=" ")
 
 pcd_vector = o3d.geometry.PointCloud()
-# 加载点坐标
+
 pcd_vector.points = o3d.utility.Vector3dVector(pcd[:, :3])
 o3d.visualization.draw_geometries([pcd_vector])
 
